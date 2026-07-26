ba3d5ee7a78dfdfaa805f122ef9c155882b10090
Acriid <wilrichgerdus@gmail.com>
2026-07-26 14:45:50 +0200

    Finished the laser

--- The Void Rains Upon Her Heart - Clone/Assets/Scripts/Lasers/Laser.cs
+++ The Void Rains Upon Her Heart - Clone/Assets/Scripts/Lasers/Laser.cs
@@ -6,38 +6,87 @@ public class Laser : MonoBehaviour
 {
     [SerializeField] private LaserSO _laserSO;
     [SerializeField] private LineRenderer _laserLine;
-    public void ShootLaser(Vector2 startPosition, Vector2 endPosition)
+    const float LASERLENGTH = 20f;
+    //Laser Settings
+    private Vector2 _laserSize;
+    private Vector2 _laserStartPosition;
+    private Vector2 _laserMiddle;
+    private Vector2 _laserEndPosition;
+    private Transform _shootingTransform;
+    private float _laserAngle;
+    public void ShootLaser(Transform shootingTransform)
     {
+        _laserSO.PlayerMask = LayerMask.GetMask("Player");
+        _shootingTransform = shootingTransform;
+        _laserSize = new(LASERLENGTH,_laserSO.LaserWidth);
+
+        UpdateLaserPosition();
+
+        StartCoroutine(InitialLaserShot());
+    }
+    private IEnumerator InitialLaserShot()
+    {
+
+        float initialWidth = _laserSO.LaserWidth / 10f;
+
         _laserLine.enabled = true;
 
-        _laserLine.SetPosition(0,startPosition);
-        _laserLine.SetPosition(1,endPosition);
 
-        _laserLine.startWidth = 0.25f;
-        _laserLine.endWidth = 0.25f;
+        SetLaserPosition();
 
-        StartCoroutine(ShootLaserEnumerator());
-    }
 
-    private IEnumerator ShootLaserEnumerator()
-    {
+        _laserLine.startWidth = initialWidth;
+        _laserLine.endWidth = initialWidth;
 
-        yield return new WaitForSeconds(_laserSO.LaserShootDelay);
+        float timeTracker = 0f;
+        while(timeTracker < _laserSO.LaserShootDelay)
+        {
+            UpdateLaserPosition();
+            timeTracker += Time.deltaTime;
+            yield return null;
+        }
 
         _laserLine.startWidth = _laserSO.LaserWidth;
         _laserLine.endWidth = _laserSO.LaserWidth;
 
         StartCoroutine(ContinuosLaserShot());
+
     }
 
+    private void UpdateLaserPosition()
+    {
+        _laserStartPosition = _shootingTransform.position;
+        _laserEndPosition =  _shootingTransform.position + _shootingTransform.up.normalized * LASERLENGTH;
+        _laserMiddle = (_laserStartPosition + _laserEndPosition) * 0.5f;
+
+        _laserAngle = Vector2.SignedAngle(Vector2.right,_shootingTransform.up);
+
+        SetLaserPosition();
+    }
     private IEnumerator ContinuosLaserShot()
     {
         float timeTracker = 0f;
+
         while(timeTracker < _laserSO.LaserLifetime)
         {
+            Collider2D hit = Physics2D.OverlapBox(_laserMiddle,_laserSize,_laserAngle,_laserSO.PlayerMask);
+            if(hit)
+            {
+                Debug.Log("Hit thing");
+            }
+
+            UpdateLaserPosition();
+
             timeTracker += Time.deltaTime;
+
             yield return null;
         }
+
         _laserLine.enabled = false;
+    }
+    private void SetLaserPosition()
+    {
+        _laserLine.SetPosition(0,_laserStartPosition);
+        _laserLine.SetPosition(1,_laserEndPosition);  
     }
 }
--- The Void Rains Upon Her Heart - Clone/Assets/Scripts/Temp (Delete Later)/TestSectionMove.cs
+++ The Void Rains Upon Her Heart - Clone/Assets/Scripts/Temp (Delete Later)/TestSectionMove.cs
@@ -5,10 +5,36 @@ public class TestSectionMove : MonoBehaviour
 {
     public BossBrain BossBrain;
     public Laser TestLaser;
+    public float speed = 100f;
+
+    [SerializeField] private float _sectionSpeed = 4f;
+    [SerializeField] private float _movementDistance = 2f;
+    [SerializeField] private float _phaseOffset = 0f;
+
+    private Vector2 _startPosition;
 
     void Awake()
     {
         BossBrain.StartSinAttack();
-        TestLaser.ShootLaser(Vector2.zero,new(10f,10f));
+        TestLaser.ShootLaser(transform);
+
+        QualitySettings.vSyncCount = 0;
+
+        
+        Application.targetFrameRate = 60;
+
+        _startPosition = transform.position;
     }
+
+    void Update()
+    {
+        //transform.Rotate(0f, 0f, speed * Time.deltaTime);
+
+
+        float movementOffset = Mathf.Sin(Time.time * _sectionSpeed + _phaseOffset) * _movementDistance;
+
+        transform.position = _startPosition + Vector2.up * movementOffset;
+    }
+
+
 }
