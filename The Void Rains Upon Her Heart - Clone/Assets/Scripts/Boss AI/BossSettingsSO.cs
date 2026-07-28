08bbbf2d4a9ba7401913db515b1ec71f8c2ffea6
Acriid <wilrichgerdus@gmail.com>
2026-07-28 14:27:46 +0200

    added bossSO with just boss health

---
+++ The Void Rains Upon Her Heart - Clone/Assets/Scripts/Boss AI/BossSettingsSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BossSettings", menuName = "Boss/BossSettings")]
+public class BossSettingsSO : ScriptableObject
+{
+    public int BossHealth = 1929;
+}
