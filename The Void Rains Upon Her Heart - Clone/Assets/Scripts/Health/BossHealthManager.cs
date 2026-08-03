d30ccc6c398e67c27a0c8bd75ad528d519b449e0
afancyfrog <tanyafreese@gmail.com>
2026-08-03 14:50:18 +0200

    import assets

--- The Void Rains Upon Her Heart - Clone/Assets/Scripts/Health/BossHealthManager.cs
+++ The Void Rains Upon Her Heart - Clone/Assets/Scripts/Health/BossHealthManager.cs
@@ -1,19 +1,30 @@
+using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHealthManager : MonoBehaviour
 {
    [SerializeField] private float _maxHealth = 0f;
-   [SerializeField] private BossBrain _bossBrain;
+    [SerializeField] private float _currentHealth = 0f;
+    [SerializeField] private BossBrain _bossBrain;
    private List<BossSection> _bossSections;
+    [SerializeField] private Slider _HealthSlider;
+    [SerializeField] private Slider _DamageSlider;
+
 
     void Awake()
     {
         _maxHealth = _bossBrain.GetMaxHealth();
         _bossSections = _bossBrain.GetBossSections();
 
-        foreach(BossSection bossSection in _bossSections)
+        _currentHealth = _maxHealth;
+        _HealthSlider.maxValue = _maxHealth;
+        _HealthSlider.value = _currentHealth;
+        _DamageSlider.maxValue = _maxHealth;
+        _DamageSlider.value = _currentHealth;
+
+        foreach (BossSection bossSection in _bossSections)
         {
             bossSection.OnBossDamage += TakeDamage;
         }
@@ -28,6 +39,51 @@ public class BossHealthManager : MonoBehaviour
     }
     private void TakeDamage(float damage)
     {
-        _maxHealth -= damage;
+        _currentHealth -= damage;
+        
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth); //makes sure current health doesnt fall below 0
+
+    
+
+        if (_currentHealth <= 0)
+        {
+           BossDeath();
+        }
+
+        StartCoroutine(HealthBarAnimation());
+
     }
+
+    IEnumerator HealthBarAnimation()
+    {
+       //flash White
+        //add shakes when health low
+        while (_HealthSlider.value!>= _currentHealth)
+        {
+            _HealthSlider.value -= 1f;
+            yield return new WaitForSeconds(0.05f);
+
+        }
+
+    }
+
+    IEnumerator HealthBarAnimationEnd()
+    {
+        //_HealthSlider.value = _currentHealth;
+        yield return new WaitForSeconds(2f);
+        //add shakes when health low
+        while (_HealthSlider.value! <= _DamageSlider.value)
+        {
+            _DamageSlider.value -= 10f;
+            yield return new WaitForSeconds(0.05f);
+
+        }
+
+    }
+
+    private void BossDeath()
+    {
+        StartCoroutine(HealthBarAnimationEnd());
+    }
+
 }
